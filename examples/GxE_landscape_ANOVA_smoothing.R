13268505cffe41abc4fad9384b4e18c530d52a97
MartinBoer <martin.boer@wur.nl>
2020-07-15 11:10:55 +0200

    example uses the updated package.

--- examples/GxE_landscape_ANOVA_smoothing.R
+++ examples/GxE_landscape_ANOVA_smoothing.R
@@ -7,7 +7,6 @@ rm(list=ls())
 
 library(ggplot2)
 library(sampling)
-library(splines)
 library(LMMsolver)
 
 set.seed(1234)
@@ -16,7 +15,7 @@ set.seed(1234)
 sigma2 <- 4.0
 
 # simulated surface a rotated ellipse
-f <- function(x1,x2, theta) {
+simfun <- function(x1,x2, theta) {
   a = 0.2
   b = 0.3
   x1c = 0.4
@@ -28,6 +27,9 @@ f <- function(x1,x2, theta) {
 
 # rotation of the simulated ellipse surface:
 degrees_rotation = 30
+
+# rotation in radials
+rotation = (degrees_rotation/180)*pi
 
 x1grid <- (c(1:100)-0.5)/100
 x2grid <- (c(1:100)-0.5)/100
@@ -45,12 +47,7 @@ ndx1 <- strata(data.frame(x1f),stratanames="x1f",size=rep(1,times=nEnv),method='
 x2f <- findInterval(x1grid,seq(from=0,to=1,length=nGeno+1))
 ndx2 <- strata(data.frame(x2f),stratanames="x2f",size=rep(1,times=nGeno),method='srswor')$ID_unit
 
-grid$ysim <- f(grid$x1, grid$x2, theta=(degrees_rotation/180)*pi)
-
-#env_cov <- runif(nEnv,min=0.02,max=0.98)
-#geno_cov <- runif(nGeno,min=0.02,max=0.98)
-#env_cov <- seq(from=0.05, to=0.95, length=nEnv)
-#geno_cov <- seq(from=0.05, to=0.95, length=nGeno)
+grid$ysim <- simfun(grid$x1, grid$x2, theta=rotation)
 
 env_cov <- x1grid[ndx1]
 geno_cov <- x2grid[ndx2]
@@ -64,7 +61,7 @@ geno_df <- data.frame(gen,x=geno_cov)
 sim.df <- expand.grid(env_cov, geno_cov)
 names(sim.df) <- c("x1","x2")
 Nobs <- nrow(sim.df)
-sim.df$ysim <- f(sim.df$x1, sim.df$x2, theta=(degrees_rotation/180)*pi)
+sim.df$ysim <- simfun(sim.df$x1, sim.df$x2, theta= rotation)
 e <- rnorm(Nobs, mean = 0, sd = sqrt(sigma2))
 sim.df$y <- sim.df$ysim + e
 
@@ -79,69 +76,29 @@ ggplot(grid)+
   theme(plot.title = element_text(hjust = 0.5)) +
   coord_equal()
 
-# row wise kronecker product
-Rten2 <-
-  function(X1,X2) {
-    one.1 <- matrix(1,1,ncol(X1))
-    one.2 <- matrix(1,1,ncol(X2))
-    kronecker(X1,one.2)*kronecker(one.1,X2)
-  }
 
 degree = 3
-k = degree+1
+pord <- 1
 x1min = 0
 x1max = 1
 x2min = 0
 x2max = 1
 nseg1 = 10
 nseg2 = 8
-#nseg1 = 25
-#nseg2 = 30
-
-dx1 = (x1max - x1min) / nseg1
-dx2 = (x2max - x2min) / nseg2
 
 x1 = sim.df$x1
 x2 = sim.df$x2
 
-knots1 = seq(x1min - degree * dx1, x1max + degree * dx1, by = dx1)
-knots2 = seq(x2min - degree * dx2, x2max + degree * dx2, by = dx2)
+knots1 = PsplinesKnots(x1min, x1max, degree, nseg1)
+knots2 = PsplinesKnots(x2min, x2max, degree, nseg2)
 
-#knots1 <- c(rep(x1min,3),seq(x1min,x1max,by=dx1),rep(x1max,3))
-#knots2 <- c(rep(x2min,3),seq(x2min,x2max,by=dx2),rep(x2max,3))
+B1 <- Bsplines(knots1, x1)
+B2 <- Bsplines(knots2, x2)
+q1 <- ncol(B1)
+q2 <- ncol(B2)
 
-B1 = splineDesign(knots1, x1, derivs=rep(0,length(x1)), ord = degree+1)
-q1 = ncol(B1)
-q1
-
-B2 = splineDesign(knots2, x2, derivs=rep(0,length(x2)), ord = degree+1)
-q2 = ncol(B2)
-q2
-
-D1 <- diff(diag(q1), diff=1)
-DtD1 <- crossprod(D1)
-U1 <- eigen(DtD1)$vectors[,-q1]
-d1 <- eigen(DtD1)$values[-q1]
-U1sc <- U1 %*% diag(1/sqrt(d1))
-
-D2 <- diff(diag(q2), diff=1)
-DtD2 <- crossprod(D2)
-U2 <- eigen(DtD2)$vectors[,-q2]
-d2 <- eigen(DtD2)$values[-q2]
-U2sc <- U2 %*% diag(1/sqrt(d2))
-
-# J1 = matrix(data=1,ncol=q1,nrow=q1)
-# K1 = diag(q1)- J1
-# U1 <- eigen(K1)$vectors[,-q1]
-# d1 <- eigen(K1)$values[-q1]
-# U1sc <- U1 %*% diag(1/sqrt(d1))
-#
-# J2 = matrix(data=1,ncol=q2,nrow=q2)
-# K2 = diag(q2)- J2
-# U2 <- eigen(K2)$vectors[,-q2]
-# d2 <- eigen(K2)$values[-q2]
-# U2sc <- U2 %*% diag(1/sqrt(d2))
-
+U1sc <- calcUsc(q1, pord)
+U2sc <- calcUsc(q2, pord)
 U12sc = kronecker(U1sc, U2sc)
 
 df <- data.frame(gen=rep(gen,each=nEnv),
@@ -151,39 +108,38 @@ df_csv <- data.frame(gen=rep(gen,each=nEnv),
                 env=rep(env,times=nGeno),
                 y=sim.df$y, x=sim.df$x2,z=sim.df$x1)
 head(df_csv)
-write.csv(df_csv,"GxE_simulated_surface.csv",quote=FALSE,row.names=FALSE)
+#write.csv(df_csv,"GxE_simulated_surface.csv",quote=FALSE,row.names=FALSE)
 
 head(df)
+
+# model without interaction:
 
 lZ <- list()
 lZ[[1]] = B1 %*% U1sc
 lZ[[2]] = B2 %*% U2sc
-Z <- do.call("cbind",lZ)
+Z <- do.call("cbind", lZ)
 
-df_ext = cbind(df,Z)
+df_ext = cbind(df, Z)
 
-dim <- sapply(lZ,ncol)
-e <- cumsum(dim) + ncol(df)
-s <- e - dim + 1
-
-# model without interactions:
-lM <- list(E=c(s[1]:e[1]), G=c(s[2]:e[2]))
+lM <- ndxMatrix(lZ, c("E","G"))
 obj0 = LMMsolve(fixed=y~-1+InterceptMB, random=NULL,randomMatrices=lM, data=df_ext)
 obj0$logL
 obj0$ED
 
-lZ[[3]] = Rten2(B1,B2) %*% U12sc
+# model with GxE interaction:
+lZ <- list()
+lZ[[1]] = B1 %*% U1sc
+lZ[[2]] = B2 %*% U2sc
+lZ[[3]] = RowKronecker(B1,B2) %*% U12sc
 Z <- do.call("cbind",lZ)
 df_ext = cbind(df,Z)
-dim <- sapply(lZ,ncol)
-e <- cumsum(dim) + ncol(df)
-s <- e - dim + 1
 
-lM <- list(E=c(s[1]:e[1]), G=c(s[2]:e[2]), GxE=c(s[3]:e[3]))
-
+lM <- ndxMatrix(lZ, c("E","G","GxE"))
 obj1 = LMMsolve(fixed=y~-1+InterceptMB, random=NULL,randomMatrices=lM, data=df_ext)
 obj1$logL
 obj1$ED
+
+# just check the sum to zero constraints:
 
 theta1 <- U1sc %*% coef(obj1)$E
 theta2 <- U2sc %*% coef(obj1)$G
@@ -196,55 +152,9 @@ M = matrix(data=theta3,nrow=q1,ncol=q2,byrow=TRUE)
 rowSums(M)
 colSums(M)
 
-B1x = splineDesign(knots1, x1grid, derivs=rep(0,length(x1grid)), ord = degree+1)
-B2x = splineDesign(knots2, x2grid, derivs=rep(0,length(x2grid)), ord = degree+1)
-
-nGridext = 500
-x1grid_ext = seq(x1min - degree * dx1, x1max + degree * dx1,length=nGridext)
-x2grid_ext = seq(x2min - degree * dx2, x2max + degree * dx2,length=nGridext)
-#x1grid_ext = seq(x1min, x1max,length=nGridext)
-#x2grid_ext = seq(x2min, x2max,length=nGridext)
-
-B1x_ext = splineDesign(knots1, x1grid_ext, derivs=rep(0,length(x1grid_ext)), ord = degree+1,outer.ok=TRUE)
-B2x_ext = splineDesign(knots2, x2grid_ext, derivs=rep(0,length(x2grid_ext)), ord = degree+1,outer.ok=TRUE)
-
-# marginals
-sum(B1x_ext %*% theta1)
-sum(B2x_ext %*% theta2)
-plot(x=x1grid_ext,y=B1x_ext %*% theta1,type='l')
-abline(h=0,col='red')
-abline(v=x1min,lt=2)
-abline(v=x1max,lt=2)
-
-plot(x=x2grid_ext,y=B2x_ext %*% theta2,type='l')
-abline(h=0,col='red')
-abline(v=x1min,lt=2)
-abline(v=x1max,lt=2)
-
-
-ypred_ext <- kronecker(B1x_ext,B2x_ext) %*% theta3
-M = matrix(ypred_ext,ncol=nGridext,nrow=nGridext)
-range(rowSums(M))
-range(colSums(M))
-
-k1 <- length(x1grid_ext)
-k2 <- length(x2grid_ext)
-x1_ext <- kronecker(x1grid_ext, rep(1,k2))
-x2_ext <- kronecker(rep(1,k1), x2grid_ext)
-
-pred_ext <- data.frame(x1=x1_ext,x2=x2_ext,y=kronecker(B1x_ext,B2x_ext)%*%theta3)
-
-ggplot(pred_ext)+
-  geom_raster(mapping=aes(x=x1,y=x2,fill=y))+
-  scale_fill_gradientn(name="Fitted",colours=topo.colors(100)) +
-  #  scale_fill_gradient(low = "blue", high = "red",name="Fitted") +
-  geom_vline(xintercept=c(x1min,x1max)) +
-  geom_hline(yintercept=c(x2min,x2max)) +
-  geom_point(sim.df,mapping=aes(x=x1,y=x2),size=2)+
-  ggtitle("GxE _extended") + xlab("z") + ylab("x") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  coord_equal()
-
+# make predictions on a dense grid:
+B1x = Bsplines(knots1, x1grid)
+B2x = Bsplines(knots2, x2grid)
 
 k1 <- length(x1grid)
 k2 <- length(x2grid)
@@ -259,45 +169,36 @@ pred2 <- data.frame(x2=x2grid, y=eff2)
 ggplot(data=pred1, aes(x=x1, y=eff1)) + geom_line()
 ggplot(data=pred2, aes(x=x2, y=eff2)) + geom_line()
 
-yfit = kronecker(B1x,B2x) %*% theta3
+# calculate the predictions on the grid:
+fit_E <- kronecker(eff1,rep(1,k2))
+fit_G <- kronecker(rep(1,k1), eff2)
+fit_GxE = kronecker(B1x,B2x) %*% theta3
+fit_GGE = fit_G + fit_GxE
+yfit = mu + fit_E + fit_G + fit_GxE
 
-x1 <- kronecker(x1grid, rep(1,k2))
-x2 <- kronecker(rep(1,k1), x2grid)
-pred <- data.frame(x1=x1,
-                   x2=x2,
-                   y = yfit)
+x1_ext <- kronecker(x1grid, rep(1,k2))
+x2_ext <- kronecker(rep(1,k1), x2grid)
+pred <- data.frame(x1=x1_ext,x2=x2_ext, fit_G, fit_GxE, fit_GGE, yfit)
 
 ggplot(pred)+
-  geom_raster(mapping=aes(x=x1,y=x2,fill=y))+
+  geom_raster(mapping=aes(x=x1,y=x2,fill=fit_GxE))+
   scale_fill_gradientn(name="Fitted",colours=topo.colors(100)) +
-  #  scale_fill_gradient(low = "blue", high = "red",name="Fitted") +
   geom_point(sim.df,mapping=aes(x=x1,y=x2),size=2)+
   ggtitle("GxE") + xlab("z") + ylab("x") +
   theme(plot.title = element_text(hjust = 0.5)) +
   coord_equal()
 
-yfit = kronecker(rep(1,k1), eff2) + kronecker(B1x,B2x) %*% theta3
-x1 <- kronecker(x1grid, rep(1,k2))
-x2 <- kronecker(rep(1,k1), x2grid)
-pred <- data.frame(x1=x1, x2=x2, y = yfit)
-
-ggplot(pred, aes(x1,x2,z=y)) +
-  geom_raster(mapping=aes(x=x1,y=x2,fill=y))+
+ggplot(pred) +
+  geom_raster(mapping=aes(x=x1,y=x2,fill=fit_GGE))+
   scale_fill_gradientn(name="Fitted",colours=topo.colors(100)) +
-  #  scale_fill_gradient(low = "blue", high = "red",name="Fitted") +
   geom_point(sim.df,mapping=aes(x=x1,y=x2),size=2)+
   ggtitle("G + GxE") + xlab("z") + ylab("x") +
   theme(plot.title = element_text(hjust = 0.5)) +
   coord_equal()
 
-yfit = mu + kronecker(eff1,rep(1,k2)) + kronecker(rep(1,k1), eff2) + kronecker(B1x,B2x) %*% theta3
-
-pred <- data.frame(x1=x1, x2=x2, y = yfit)
-
 ggplot(pred)+
-  geom_raster(mapping=aes(x=x1,y=x2,fill=y))+
+  geom_raster(mapping=aes(x=x1,y=x2,fill=yfit))+
   scale_fill_gradientn(name="Fitted",colours=topo.colors(100)) +
-#  scale_fill_gradient(low = "blue", high = "red",name="Fitted") +
   geom_point(sim.df,mapping=aes(x=x1,y=x2),size=2)+
   ggtitle("E + G + GxE") + xlab("z") + ylab("x") +
   theme(plot.title = element_text(hjust = 0.5)) +
