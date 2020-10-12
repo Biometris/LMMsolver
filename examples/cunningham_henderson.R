32a53c9f8a3d50dae499b194fec94b5b78d1bf10
MartinBoer <martin.boer@wur.nl>
2020-10-12 15:10:02 +0200

    nlme and mgcv added for comparison

--- examples/cunningham_henderson.R
+++ examples/cunningham_henderson.R
@@ -7,6 +7,8 @@
 rm(list=ls())
 library(asreml)
 library(LMMsolver)
+library(mgcv)
+library(nlme)
 library(dplyr)
 
 dat <- read.csv("cunningham_henderson_ex.csv")
@@ -16,7 +18,7 @@ head(dat)
 dat %>% group_by(treatment, block) %>% tally()
 
 # asreml for comparison with LMMsolver
-obj1 <- asreml(y~treatment, random = ~block,data=dat)
+obj1 <- asreml(y~treatment, random = ~block, data=dat)
 obj1$loglik
 
 coef(obj1, list=TRUE)
@@ -29,6 +31,29 @@ obj2$logL
 obj2$ED
 
 coef(obj2)
+
+# nlme4:
+obj3 <- lme(y~treatment,data=dat,~1|block)
+
+# mgcv:
+obj4 <- gam(y ~ treatment + s(block,bs="re"), data=dat, method="REML")
+summary(obj4)
+
+#
+# Compare the logL for the four models (asreml, LMMsolver, nlme, mgcv) used,
+# should be all equal:
+#
+p <- nlevels(dat$treatment)
+N <- nrow(dat)
+
+Constant_logL <- -0.5*log(2*pi)*(N-p)
+
+round(obj1$loglik + Constant_logL,3)
+round(obj2$logL + Constant_logL, 3)
+round(obj3$logLik, 3)
+round(as.numeric(-obj4$gcv.ubre),3)
+
+
 
 
 
