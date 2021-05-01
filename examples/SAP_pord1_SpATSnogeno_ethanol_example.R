1b2cc40849b02593bd269f47179fada42b59cec8
Martin Boer <martin.boer@wur.nl>
2021-05-01 11:47:46 +0200

    some cleaning code sparse SAP example.

--- examples/SAP_pord1_SpATSnogeno_ethanol_example.R
+++ examples/SAP_pord1_SpATSnogeno_ethanol_example.R
@@ -3,7 +3,6 @@
 #' author: Martin Boer, Biometris, WUR, Wageningen, the Netherlands
 #' ---
 
-
 rm(list=ls())
 
 library(spam)
@@ -26,7 +25,7 @@ pord <- 1
 
 # Fit the PS-ANOVA model
 obj1 <- SpATS.nogeno(response = "NOx",
-                     spatial = ~SAP(E, C,  nseg = nseg, pord=pord),
+                     spatial = ~SAP(E, C, nseg = nseg, degree=degr, pord=pord),
                      data = ethanol,
                      control = list(maxit = 100, tolerance = 1.0e-10,
                                     monitoring = 2, update.psi = FALSE))
@@ -44,9 +43,11 @@ points(ethanol$C, ethanol$E, pch = '+')
 #' solve using sparse formulation SAP
 #' ==================
 
-dat <- ethanol
-x1 <- dat$E
-x2 <- dat$C
+x1 <- ethanol$E
+x2 <- ethanol$C
+y <- ethanol$NOx
+
+s <- proc.time()[3]
 
 knots1 <- PsplinesKnots(min(x1), max(x1), degree=degr, nseg=nseg[1])
 knots2 <- PsplinesKnots(min(x2), max(x2), degree=degr, nseg=nseg[2])
@@ -65,31 +66,23 @@ DtD2 <- crossprod(D2)
 # we have to calculate RowKronecker product only once:
 B21 <- RowKronecker(B2, B1)
 
-Nind <- nrow(dat)
-Nblocks <- nlevels(dat$plotId)
-X<-matrix(data=1,ncol=1,nrow=Nind)
+Nobs <- length(y)
+X<-matrix(data=1, ncol=1, nrow=Nobs)
 
 # make Rinv
-#Nind <- nrow(X)
 lRinv = list()
-lRinv[[1]] = diag.spam(1,Nind)
+lRinv[[1]] = diag.spam(1, Nobs)
 names(lRinv) = "residual"
 
 Z <- B21
 
-dim.r = c(ncol(B21), Nblocks)
-e <- cumsum(dim.r)
-s <- e - dim.r + 1
-
-# change the constraints, rest same as before..
-C2 <- c(1, rep(0,q1*q2-2), 1)
-CCt2 <- tcrossprod(C2)
+# boundary constraint
+C <- c(1, rep(0,q1*q2-2), 1)
+CCt <- tcrossprod(C)
 lGinv <- list()
-lGinv[[1]] <- bdiag.spam(as.spam(kronecker.spam(diag(q2), DtD1) + CCt2))
-lGinv[[2]] <- bdiag.spam(as.spam(kronecker.spam(DtD2, diag(q1)) + CCt2))
-
-y <- dat$NOx
-s <- proc.time()[3]
+lGinv[[1]] <- bdiag.spam(as.spam(kronecker.spam(diag(q2), DtD1) + CCt))
+lGinv[[2]] <- bdiag.spam(as.spam(kronecker.spam(DtD2, diag(q1)) + CCt))
+names(lGinv) <- c('f(E,C)|E', 'f(E,C)|C')
 obj2 = sparseMixedModels(y,X,Z,lGinv,lRinv,maxiter=300,
                           eps=1.0e-10,display=TRUE,monitor=TRUE)
 e <- proc.time()[3]
