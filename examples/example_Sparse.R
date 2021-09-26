f4bbc8c9401f2546c191c0b404ea90acf2be6c26
Martin Boer <martin.boer@wur.nl>
2021-09-26 22:11:06 +0200

    extension of example with problem if fixed effect only intercept

--- examples/example_Sparse.R
+++ examples/example_Sparse.R
@@ -4,27 +4,32 @@ library(nlme)
 set.seed(1234)
 
 # number of levels
-K <- 100
+K <- 1000
 r <- 5
 n <- K*r
 
 s <- rnorm(K, sd=1.0)
-e <- rnorm(n, sd=1.0)
+e <- rnorm(n, sd=5.0)
 
 y = rep(s,r) + e
 
-df <- data.frame(y,block=as.factor(rep(paste0("blk",1:K), r)))
+df <- data.frame(y,block= as.factor(rep(paste0("blk",1:K), r)),
+                   rep  = as.factor(rep(paste0("rep",1:r), each=K)))
 head(df)
 str(df)
 
-obj1 <- LMMsolve(y~1,random=~block, data=df, trace=TRUE,display=TRUE)
+# This model doesn't work for K > 1000, problem with intercept:
+# obj0 <- LMMsolve(y~1,random=~block, data=df, trace=TRUE)
 
+# This model is ok, with replicate fixed:
+obj1 <- LMMsolve(y~rep,random=~block, data=df, trace=TRUE)
+obj1$ED
 # nlme4:
-obj2 <- lme(y~1,data=df,~1|block)
+obj2 <- lme(y~rep,data=df,~1|block)
 
 obj1$logL
 
-p <- 1
+p <- r
 N <- nrow(df)
 
 Constant_logL <- -0.5*log(2*pi)*(N-p)
