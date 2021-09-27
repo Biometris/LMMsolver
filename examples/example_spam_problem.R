b26698065d8b02f860b4b3cb88d2213aae5dba00
Martin Boer <martin.boer@wur.nl>
2021-09-27 11:43:26 +0200

    some further explanation code.

--- examples/example_spam_problem.R
+++ examples/example_spam_problem.R
@@ -1,7 +1,7 @@
-
+rm(list=ls())
 library(spam)
 
-K <- 100
+K <- 10
 r <- 2
 lambda <- 1.0
 B <- diag.spam(r+lambda, nrow=K+1)
@@ -10,10 +10,14 @@ B[1,] <- r
 B[1, 1] <- K*r
 B[1:4, 1:4]
 
+# for K <= 500 ok, for K > 500 the function crashes often,
+# there seems to be problem with memory allocation:
 cholB1 <- spam::chol(B)
 str(cholB1)
+
+# this is ok...
 cholB2 <- spam::chol(B, memory=list(nnzcolindices=50000))
 
-str(cholB)
+str(cholB2)
 
 sessionInfo()
