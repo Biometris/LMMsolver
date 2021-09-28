138f7bab31609abb07c35e851652e96f3e37134f
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2021-09-28 16:26:27 +0200

    Readded test for spl1D (#14)

---
+++ data_raw/testdata.R
@@ -0,0 +1,6 @@
+library(agridat)
+
+data(john.alpha)
+
+## Save to tinytest directory.
+save(john.alpha, file = "./inst/tinytest/testdata.rda")
---
+++ inst/tinytest/test_spl1D.R
@@ -0,0 +1,25 @@
+load("testdata.rda")
+
+n <- 4         # number of units per block
+b <- 6         # number of blocks per replicate
+r <- 3         # number of replicates
+v <- 24        # number of genotypes/replicate:
+N <- n * b * r # total number of observations.
+
+# Here scaleX is FALSE in spl1D, to be consistent with model in JABES2020 paper.
+obj1 <- LMMsolve(fixed = yield ~ rep + gen,
+                 spline = ~spl1D(x = plot, nseg = N - 1, degree = 1, pord = 1,
+                                 scaleX = FALSE),
+                 data = john.alpha,
+                 trace = FALSE,
+                 tolerance = 1.0e-10)
+
+## calculate deviance, as in JABES 2020 paper, table 1,
+## include the constant omitted in LMMsolver (and asreml, SpATS).
+p <- 1 + (v - 1) + (r - 1)
+Constant = log(2 * pi) * (N - p)
+dev = -2 * obj1$logL + Constant
+
+## compare with JABES2020 paper.
+devJABES2020paper <- 54.49
+expect_equal(round(dev, 2), devJABES2020paper)
