32a6a2c4a65af3e7ad2c010d24c2bbe5d1aeb408
Bustos Korts <daniela.bustoskorts@wur.nl>
2021-10-13 16:37:49 +0200

    Update Example_LMMsolver_PAR_NPEC.R

--- examples/Example_LMMsolver_PAR_NPEC.R
+++ examples/Example_LMMsolver_PAR_NPEC.R
@@ -28,6 +28,10 @@ rowdist <- 24
 coldist <- 58
 pos$colm <- (as.numeric(pos$colnr)) * coldist - coldist
 pos$rowm <- as.numeric(pos$rownr) * rowdist - rowdist
+head(input)
+x1 <- input$row
+x2 <- input$col
+x3 <- input$time
 
 #### Using new way of calling LMMsolve:
 
@@ -35,6 +39,7 @@ pos$rowm <- as.numeric(pos$rownr) * rowdist - rowdist
 x1lim <- c(min(x1) - 0.01, max(x1) + 0.01)
 x2lim <- c(min(x2) - 0.01, max(x2) + 0.01)
 x3lim <- c(min(x3) - 0.01, max(x3) + 0.01)
+length(unique(input$time))
 
 fittingNew <- LMMsolve(fixed = par~1,
                        spline = ~spl3D(row, col, time, nseg = c(2, 2, 50),
@@ -48,13 +53,14 @@ fitting2$edf
 fittingNew$ED
 
 #Prediction grid
-g2 <- expand.grid(row = (c(1:35)*24-24),
-                  col = (c(1:14)*58-58),
+g2 <- expand.grid(row = (c(1:max(pos$rownr))*rowdist-rowdist),
+                  col = (c(1:max(pos$colnr))*coldist-coldist),
                   time = seq(min(x3), max(x3),
                              by = 0.02))
 
 
-pred2 <- obtainSmoothTrend(fittingNew, newdata = g2, includeIntercept = TRUE)
+pred2 <- obtainSmoothTrend(fittingNew, newdata = g2,
+                           includeIntercept = TRUE)
 
 # Plot predictions
 for (j in unique(pred2$time)) {
@@ -67,8 +73,10 @@ for (j in unique(pred2$time)) {
   image.plot(t(X1), x = (unique(g2$col)), y = unique(g2$row),
              col = wes_palette("Zissou1", 100, type = "continuous"),
              xlab='column',ylab='row',
-             main = paste0('Temp ', as_datetime(j*100000) ),
+             main = paste0('Temp ', as_datetime(j*100000)),
              zlim = c(min(pred2$ypred),max(pred2$ypred)))
   text(y=pos$rowm, x= pos$colm, col = 'black', pos$coord)
 }
+
+
 
