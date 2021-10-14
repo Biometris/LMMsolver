f96b12ec811df85fccf8a0102a89acfd0d921f36
Martin Boer <martin.boer@wur.nl>
2021-10-14 18:45:16 +0200

    description of multipop simulated data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' simulated QTL mapping data set
+#'
+#' A data.frame with 180 rows and 6 columns.\cr
+#' \describe{
+#' \item{cross}{Cross ID, two populations, AxB and AxC}
+#' \item{ind}{Genotype ID}
+#' \item{pA}{Probability that individual has alleles from parent A}
+#' \item{pB}{Probability that individual has alleles from parent B}
+#' \item{pC}{Probability that individual has alleles from parent C}
+#' \item{pheno}{simulated phenotypic value}
+#' }
+#'
+"multipop"
+
