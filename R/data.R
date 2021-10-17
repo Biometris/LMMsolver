7a8997a804f9f4cfc21ce6e57776d24697b522b8
Martin Boer <martin.boer@wur.nl>
2021-10-17 16:42:57 +0200

    Logistic curve replaced by APSIM data (1 genotype / 1 env).

--- R/data.R
+++ R/data.R
@@ -10,3 +10,16 @@
 #' \item{pheno}{simulated phenotypic value}
 #' }
 "multipop"
+
+#' Simulated Biomass as function of time using APSIM wheat.
+#' @format A data.frame with 121 rows and 4 columns.
+#' \describe{
+#' \item{env}{environment, Emerald in 1993}
+#' \item{geno}{simulated genotype g001}
+#' \item{das}{days after sowing}
+#' \item{biomass}{simulated biomass using APSIM; medium measurement error added}
+#' }
+#' @references Bustos-Korts et al. (2019) Combining Crop Growth Modeling and Statistical
+#' Genetic Modeling to Evaluate Phenotyping Strategies
+#' \url{https://doi.org/10.3389/FPLS.2019.01491}
+"APSIMdat"
