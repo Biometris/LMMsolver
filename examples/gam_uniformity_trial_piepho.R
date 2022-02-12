1ff39295b0ce956d3388724c90adf7fac71a42fe
Martin Boer <martin.boer@wur.nl>
2022-02-12 13:03:53 +0100

    removed dplyr dependency

--- examples/gam_uniformity_trial_piepho.R
+++ examples/gam_uniformity_trial_piepho.R
@@ -1,16 +1,14 @@
 library(agridat)
 library(LMMsolver)
 library(ggplot2)
-library(dplyr)
+
 data("piepho.barley.uniformity")
 dat <- piepho.barley.uniformity
-dat$R <- as.factor(dat$row)
-dat$C <- as.factor(dat$col)
-dat <- filter(dat,!is.na(yield))
+dat <- dat[!is.na(dat$yield), ]
 
 obj <- LMMsolve(fixed = yield~1,
-                spline = ~spl1D(x = row, nseg = 50, degree = 3, pord = 2) +
-                  spl1D(x = col, nseg =  50, degree = 3, pord = 2),
+                spline = ~spl1D(x = row, nseg = 36, degree = 3, pord = 2) +
+                          spl1D(x = col, nseg = 30, degree = 3, pord = 2),
                 data = dat,
                 trace = FALSE)
 summary(obj)
