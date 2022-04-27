7227fd88f89094f80c48830a7a90996af341690f
Martin Boer <martin.boer@wur.nl>
2022-04-27 14:36:56 +0200

    cleaned code, use calcStandardErrors function now.

--- examples/predict_oats_data_incomplete_blocks.R
+++ examples/predict_oats_data_incomplete_blocks.R
@@ -7,12 +7,9 @@ library(ggplot2)
 library(asreml)
 library(spam)
 
-# calculate trace of matrix M:
-tr <- function(M) { sum(diag(M)) }
-
 data(john.alpha)
 dat <- john.alpha
-# remove one observation
+# remove one observation, to make example unbalanced:
 dat <- dat[-20,]
 
 obj1 <- asreml(fixed = yield~rep+gen,
@@ -41,133 +38,33 @@ pred1 <- predict(obj1, classify='gen',sed=TRUE)
 #head(pred1$pvals[1:4,])
 pred1$sed[1:3,1:3]
 
-#
-# make predictions by foot ...
-#
-N <- nrow(dat)
-y <- dat$yield
-X <- model.matrix(~rep+gen, dat)
-Z <- model.matrix(~-1+rep:block,dat)
-p <- ncol(X)
-q <- ncol(Z)
-W <- cbind(X,Z)
-Rinv <- diag.spam(1, N)
-Ginv <- diag.spam(1, q)
-P <- list()
-P[[1]] <- diag.spam(c(rep(0, p),rep(1, q)))
-P[[2]] <- as.spam(t(W) %*% W)
-sapply(P,FUN=dim)
-
-# recalculate u, should be saved in LMMsolve object:
-C <- obj2$C
-theta <- obj2$theta
-WtY <- theta[2] * t(W) %*% y
-u <- solve(C, WtY)
-
-# Replace by sparse inverse...
-Cinv <- solve(C)
-
-Nrep <- nlevels(dat$rep)
-Ngen <- nlevels(dat$gen)
-Nblk <- nlevels(dat$block)*Nrep
-
 # predictions plus standard errors,
 # averaging over fixed rep, ignoring random rep:block
 # to make this general, we have to store fixed/random info in LMMsolve object
+Nrep <- nlevels(dat$rep)
+Ngen <- nlevels(dat$gen)
+Nblk <- nlevels(dat$block)*Nrep
 Dg1 <- matrix(c(1, rep(1/Nrep, Nrep-1), 0, 0, rep(0, Ngen-3), rep(0, Nblk)), nrow=1)
 Dg2 <- matrix(c(1, rep(1/Nrep, Nrep-1), 1, 0, rep(0, Ngen-3), rep(0, Nblk)), nrow=1)
 Dg3 <- matrix(c(1, rep(1/Nrep, Nrep-1), 0, 1, rep(0, Ngen-3), rep(0, Nblk)), nrow=1)
 
-p1 <- Dg1 %*% u
-p2 <- Dg2 %*% u
-p3 <- Dg3 %*% u
-se1 <- sqrt(tr(Cinv %*% t(Dg1) %*% Dg1))
-se2 <- sqrt(tr(Cinv %*% t(Dg2) %*% Dg2))
-se3 <- sqrt(tr(Cinv %*% t(Dg3) %*% Dg3))
-
-pred2 <- c(p1, p2, p3)
-pred2se <- c(se1, se2, se3)
+Dg <- rbind.spam(Dg1, Dg2, Dg3)
+pred2se <- LMMsolver:::calcStandardErrors(obj2$C, Dg)
+pred1se <- pred1$pvals$std.error[1:3]
+range(pred1se - pred2se)
 
 # sed
 Dg12 <- Dg1-Dg2
 Dg13 <- Dg1-Dg3
 Dg23 <- Dg2-Dg3
+Dg2 <- rbind.spam(Dg12, Dg13, Dg23)
+pred2sed <- LMMsolver:::calcStandardErrors(obj2$C, Dg2)
+
 A <- matrix(data=NA,ncol=3,nrow=3)
-A[1,2] <- A[2,1] <- sqrt(tr(Cinv %*% t(Dg12) %*% Dg12))
-A[1,3] <- A[3,1] <- sqrt(tr(Cinv %*% t(Dg13) %*% Dg13))
-A[2,3] <- A[3,2] <- sqrt(tr(Cinv %*% t(Dg23) %*% Dg23))
+A[1,2] <- A[2,1] <- pred2sed[1]
+A[1,3] <- A[3,1] <- pred2sed[2]
+A[2,3] <- A[3,2] <- pred2sed[3]
 A
 
-# compare results with asreml
-pred2 - pred1$pvals$predicted.value[1:3]
-pred2se - pred1$pvals$std.error[1:3]
-A - pred1$sed[1:3,1:3]
-
-## calculate standard errors using AD chol
-
-# use AD cholesky for se:
-P <- list()
-P[[1]] <- obj2$C
-P[[2]] <- crossprod.spam(as.spam(Dg1))
-P[[3]] <- crossprod.spam(as.spam(Dg2))
-P[[4]] <- crossprod.spam(as.spam(Dg3))
-
-C <- Reduce(`+`, P)
-display(C)
-
-ADobjC <- LMMsolver:::ADchol(P)
-
-dim(C)
-dlogdet1 <- LMMsolver:::dlogdet(ADobjC,c(1.0,0.0,0.0,0.0))
-all.equal(pred2se, sqrt(dlogdet1[-1]))
-sqrt(dlogdet1[2])
-
-# use AD cholesky for sed:
-P <- list()
-P[[1]] <- obj2$C
-P[[2]] <- crossprod.spam(as.spam(Dg12))
-P[[3]] <- crossprod.spam(as.spam(Dg13))
-P[[4]] <- crossprod.spam(as.spam(Dg23))
-
-C <- Reduce(`+`, P)
-display(C)
-
-ADobjC <- LMMsolver:::ADchol(P)
-
-dim(C)
-dlogdet1 <- LMMsolver:::dlogdet(ADobjC,c(1.0,0.0,0.0,0.0))
-A2 <- matrix(data=NA,ncol=3,nrow=3)
-A2[1,2] <- A2[2,1] <- sqrt(dlogdet1[2])
-A2[1,3] <- A2[3,1] <- sqrt(dlogdet1[3])
-A2[2,3] <- A2[3,2] <- sqrt(dlogdet1[4])
-all.equal(A, A2)
-
-# calculate se using DerivCholesky() function:
-
-cholC <- obj2$cholC
-dC <- LMMsolver:::DerivCholesky(cholC)
-Dg <- rbind(Dg1,Dg2,Dg3)
-sqrt(diag(Dg %*% dC %*% t(Dg)))
-sqrt(rowSums((Dg %*% dC) * Dg))
-c(se1, se2, se3)
-
-Dg12 <- as.spam(Dg1-Dg2)
-Dg13 <- as.spam(Dg1-Dg3)
-Dg23 <- as.spam(Dg2-Dg3)
-
-# sed, add "fill-in" zero's !!
-C <- obj2$C + 0*crossprod.spam(Dg12) + 0*crossprod.spam(Dg13) + 0*crossprod.spam(Dg23)
-cholC <- chol(C)
-dC <- LMMsolver:::DerivCholesky(cholC)
-
-res12 <- sqrt(rowSums((Dg12 %*% dC) * Dg12))
-res13 <- sqrt(rowSums((Dg13 %*% dC) * Dg13))
-res23 <- sqrt(rowSums((Dg23 %*% dC) * Dg23))
-
-A3 <- matrix(data=NA,ncol=3,nrow=3)
-A3[1,2] <- A3[2,1] <- res12
-A3[1,3] <- A3[3,1] <- res13
-A3[2,3] <- A3[3,2] <- res23
-all.equal(A, A3)
-
+range(A - pred1$sed[1:3,1:3], na.rm=TRUE)
 
