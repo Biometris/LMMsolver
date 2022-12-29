3aca7300484122e2217a200a50504e21fc0002ef
Martin Boer <martin.boer@wur.nl>
2022-12-29 22:14:24 +0100

    update of geyser 2D example, detailed comparison with original JOPS implementation.

--- examples/f-geyser-aniso.R
+++ examples/f-geyser-aniso.R
@@ -1,3 +1,5 @@
+# Martin Boer, 2022, 2D density estimation using LMMsolver
+
 # Two-dimensional anisotropic density estimation (Old Faithful data)
 # A graph from the book 'Practical Smoothing. The Joys of P-splines'
 #Paul Eilers and Brian Marx, 2019
@@ -6,6 +8,7 @@ library(reshape2)
 library(ucminf)
 library(JOPS)
 library(ggplot2)
+library(LMMsolver)
 
 # Get the data
 data(faithful)
@@ -13,38 +16,33 @@ u = faithful[, 1]
 v = faithful[, 2]
 
 # Create 2-d histogram
-h = hist2d(u, v, xlim = c(1, 6), ylim = c(40, 100))
-Y = h$H
+nbins <- c(100, 100)
+h <- hist2d(u, v, nb=nbins, xlim = c(1, 6), ylim = c(40, 100))
+Y <- h$H
 
 z <- as.vector(Y)
-x <- rep(h$xgrid, times=100)
-y <- rep(h$ygrid, each=100)
-dat <- data.frame(x,y,z)
-# Turn matrix into a 'long' data frame
-#row.names(Y) = h$xgrid
-#names(Y) = h$ygrid
-#dat <- melt(Y)
-#names(dat) = c("x", "y", "z")
+x <- rep(h$xgrid, times=nbins[2])
+y <- rep(h$ygrid, each=nbins[1])
+dat <- data.frame(x, y, z)
 
-#dat2 <- dat[dat$z!=0,]
-
-# Here scaleX is FALSE in spl1D, to be consistent with model in JABES2020 paper.
+nseg <- c(10, 10)
 obj1 <- LMMsolve(fixed = z~1,
-                 spline = ~spl2D(x1 = y, x2=x, nseg = c(10,10), degree = 3, pord=2),
+                 spline = ~spl2D(x1 = x, x2=y, nseg = nseg, degree = 3, pord=2),
                  data = dat,
                  family = poisson(),
                  trace = FALSE)
 summary(obj1)
 
-pred <- obtainSmoothTrend(obj1, includeIntercept = TRUE, grid=c(100,100))
+pred <- obtainSmoothTrend(obj1, includeIntercept = TRUE, grid=nbins)
 
 family <- poisson()
-dense2 <- data.frame(x=pred$x,y=pred$y,z=sqrt(pred$ypred))
+densLMMsolver <- data.frame(x=pred$x,y=pred$y,z=sqrt(pred$ypred))
 
 # Smooth histogram and return AIC
 aicfun = function(lla) {
   lambda = 10^lla
-  fit = hist2dsm(Y, lambdax = lambda[1], lambday = lambda[2], dx = 2)
+  fit = hist2dsm(Y, nsegx=nseg[1],nsegy=nseg[2],
+                 lambdax = lambda[1], lambday = lambda[2], dx = 2)
   Mu = fit$Mu
   ok = Y > 0
   dev = 2 * sum(Y[ok] * log(Y[ok]/Mu[ok]))
@@ -56,18 +54,18 @@ aicfun = function(lla) {
 op = ucminf(c(0, 0), aicfun)
 lambda = 10^op$par
 cat("log10(lambdas), AIC:", op$par, op$value, "\n")
-fit = hist2dsm(Y, lambdax = lambda[1], lambday = lambda[2], dx = 2)
+fit = hist2dsm(Y, nsegx=nseg[1], nsegy=nseg[2],
+               lambdax = lambda[1], lambday = lambda[2], dx = 2)
 Mu = fit$Mu
 
 # Turn matrix into a 'long' data frame
-row.names(Mu) = h$xgrid
-names(Mu) = h$ygrid
-dens <- melt(sqrt(Mu))
-names(dens) = c("x", "y", "z")
-
+z <- as.vector(t(Mu))
+x <- rep(h$xgrid, each=nbins[2])
+y <- rep(h$ygrid, times=nbins[1])
+dens <- data.frame(x=x,y=y,z=sqrt(z))
 
 # Plot density with contours
-plt = ggplot(dense2, aes(x, y, fill = z)) +
+plt = ggplot(densLMMsolver, aes(x, y, fill = z)) +
   geom_raster(show.legend = F) +
   scale_fill_gradient(high = 'darkgreen', low = 'white') +
   xlab('Eruption length (min)') + ylab('Waiting time (min)') +
@@ -79,4 +77,5 @@ plt = ggplot(dense2, aes(x, y, fill = z)) +
 # Make and save plot
 plot(plt)
 
-plot(x=dens$z,y=dense2$z)
+plot(x=dens$z,y=densLMMsolver$z, main="compare methods",
+     xlab="z (JOPS)",ylab='z (LMMsolver)')
