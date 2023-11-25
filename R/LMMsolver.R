e4929aae7d311909d52e85204e8eb9107735e6ff
Martin Boer <martin.boer@wur.nl>
2023-11-25 13:58:15 +0100

    Made help for package more clear, including reference to the P-splines paper (#121)

--- R/LMMsolver.R
+++ R/LMMsolver.R
@@ -1,6 +1,22 @@
-#' @keywords internal
-"_PACKAGE"
-
+#' Linear Mixed Model Solver
+#'
+#' An efficient and flexible system to solve sparse mixed model
+#' equations, for models that are often used in statistical genetics.
+#' Important applications are the use of splines to model spatial or temporal
+#' trends. Another application area is mixed model QTL analysis for
+#' multiparental populations, allowing for heterogeneous residual variance and
+#' random design matrices with Identity-By-Descent (IBD) probabilities.
+#'
+#' @name LMMsolver
+#' @aliases LMMsolver LMMsolver-package
+#' @docType package
+#' @title Package LMMsolver
+#' @author Martin Boer \email{martin.boer@@wur.nl}
+#' @author Bart-Jan van Rossum \email{bart-jan.vanrossum@@wur.nl} (maintainer)
+#' @references Martin P. Boer (2023). \emph{Tensor product P-splines using a sparse mixed model formulation},
+#' Statistical Modelling, 23, p. 465 - 479. \url{https://doi.org/10.1177/1471082X231178591}
+#'
 #' @useDynLib LMMsolver, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+# The next and last line should be the word 'NULL'.
 NULL
