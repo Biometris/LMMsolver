2079e06ce8ee0520e8a6415ad76e624e61a8270f
Martin Boer <martin.boer@wur.nl>
2024-10-21 15:54:17 +0200

    minor, reordering some spline code in LMMsolve

--- R/LMMsolve.R
+++ R/LMMsolve.R
@@ -124,24 +124,6 @@ LMMsolve <- function(fixed,
               residual = residual, tolerance = tolerance,
               maxit = maxit, grpTheta = grpTheta)
 
-  splErr <- paste("spline should be a formula of form \"~ spl1D() + ... + ",
-                  "spl1D()\", \"~ spl2D()\" or \"~spl3D()\"\n")
-  if (!is.null(spline)) {
-    if (!inherits(spline, "formula")) stop(splErr)
-    spline <- formula(paste((gsub(pattern = "LMMsolver::",
-                                  replacement = "",
-                                  as.character(spline))), collapse = ""))
-    splTrms <- terms(spline, specials = c("spl1D", "spl2D", "spl3D"))
-    splSpec <- attr(splTrms, "specials")
-    if (length(terms(splTrms)) != 2 ||
-        ## Spline formula should consist of splxD() terms and nothing else.
-        length(unlist(splSpec)) != length(labels(terms(spline))) ||
-        length(splSpec$spl2D) > 1 ||
-        length(splSpec$spl3D) > 1) {
-      stop(splErr)
-    }
-  }
-
   ## Check that all variables used in fixed formula are in data.
   data <- checkFormVars(fixed, data, naAllowed = FALSE)
   ## Remove NA for response variable from data.
@@ -316,6 +298,25 @@ LMMsolve <- function(fixed,
     }
   } else {
     dim.f <- as.numeric(table(attr(X, "assign")))
+  }
+
+  ## Checks splines part
+  splErr <- paste("spline should be a formula of form \"~ spl1D() + ... + ",
+                  "spl1D()\", \"~ spl2D()\" or \"~spl3D()\"\n")
+  if (!is.null(spline)) {
+    if (!inherits(spline, "formula")) stop(splErr)
+    spline <- formula(paste((gsub(pattern = "LMMsolver::",
+                                  replacement = "",
+                                  as.character(spline))), collapse = ""))
+    splTrms <- terms(spline, specials = c("spl1D", "spl2D", "spl3D"))
+    splSpec <- attr(splTrms, "specials")
+    if (length(terms(splTrms)) != 2 ||
+        ## Spline formula should consist of splxD() terms and nothing else.
+        length(unlist(splSpec)) != length(labels(terms(spline))) ||
+        length(splSpec$spl2D) > 1 ||
+        length(splSpec$spl3D) > 1) {
+      stop(splErr)
+    }
   }
 
   ## Add spline part.
