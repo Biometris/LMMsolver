bf6227eeb6efb4d23453d9322594bf81b355be07
Martin Boer <martin.boer@wur.nl>
2024-11-17 21:04:29 +0100

    diagnosticsMME() and diisplayMME() functions in separate files removed from LMMsolve-class.R

---
+++ R/diagnosticsMME.R
@@ -0,0 +1,29 @@
+#' Give diagnostics for mixed model coefficient matrix C and the cholesky
+#' decomposition
+#'
+#' @param object an object of class LMMsolve.
+#'
+#' @return A summary of the mixed model coefficient matrix and its choleski
+#' decomposition.
+#'
+#' @examples
+#' ## Fit model on john.alpha data from agridat package.
+#' data(john.alpha, package = "agridat")
+#'
+#' ## Fit simple model with only fixed effects.
+#' LMM1 <- LMMsolve(fixed = yield ~ rep + gen,
+#'                 data = john.alpha)
+#'
+#' ## Obtain deviance.
+#' diagnosticsMME(LMM1)
+#'
+#' @export
+diagnosticsMME <- function(object) {
+  if (!inherits(object, "LMMsolve")) {
+    stop("object should be an object of class LMMsolve.\n")
+  }
+  cat("Summary of matrix C \n")
+  print(spam::summary.spam(object$C))
+  cat("\n Summary of cholesky decomposition of C \n")
+  print(spam::summary.spam(chol(object$C)))
+}
---
+++ R/displayMME.R
@@ -0,0 +1,33 @@
+#' Display the sparseness of the mixed model coefficient matrix
+#'
+#' @param object an object of class LMMsolve.
+#' @param cholesky Should the cholesky decomposition of the coefficient matrix
+#' be plotted?
+#'
+#' @return A plot of the sparseness of the mixed model coefficient matrix.
+#'
+#' @examples
+#' ## Fit model on john.alpha data from agridat package.
+#' data(john.alpha, package = "agridat")
+#'
+#' ## Fit simple model with only fixed effects.
+#' LMM1 <- LMMsolve(fixed = yield ~ rep + gen,
+#'                 data = john.alpha)
+#'
+#' ## Obtain deviance.
+#' displayMME(LMM1)
+#'
+#' @export
+displayMME <- function(object,
+                       cholesky = FALSE) {
+  if (!inherits(object, "LMMsolve")) {
+    stop("object should be an object of class LMMsolve.\n")
+  }
+  if (!cholesky) {
+    spam::display(object$C)
+  } else {
+    cholC <- chol(object$C)
+    L <- t(spam::as.spam(cholC))
+    spam::display(L)
+  }
+}
