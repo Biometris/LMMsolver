7673d36ad5a5f01a6002b4eee945bc64cf6d98ae
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2025-01-06 12:27:38 +0100

    Added deployment file

---
+++ examples/deployment.R
@@ -0,0 +1,42 @@
+## Check with winbuilder - develop only.
+devtools::check_win_devel()
+
+## Check with macbuilder.
+devtools::check_mac_release()
+
+rhub::platforms()
+
+## Check with rhub.
+rhub::check_for_cran(path = "C:/Projects/R_packages/LMMsolver/")
+
+## Check with rhub.
+## Some specific platforms that give issues in earlier releases.
+rhub::check_for_cran(platforms = c("ubuntu-gcc-release",
+                                   "fedora-clang-devel",
+                                   "debian-clang-devel",
+                                   "debian-gcc-devel",
+                                   "macos-highsierra-release"),
+                     path = "C:/Projects/R_packages/LMMsolver/")
+
+
+## For MAC M1 builder use this:
+## This gave test issues for version 1.0.1
+##https://mac.r-project.org/macbuilder/submit.html
+
+## Rebuild readme.
+devtools::build_readme()
+
+## Submit to CRAN
+devtools::release()
+
+## Build site for local check.
+pkgdown::clean_site()
+pkgdown::build_site()
+
+## Code coverage - local.
+detach("package:LMMsolver", unload = TRUE)
+covr::gitlab(type = "none", code = "tinytest::run_test_dir(at_home = TRUE)")
+library(LMMsolver)
+
+
+
