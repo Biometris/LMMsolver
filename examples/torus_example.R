0744eebc11c945112662b45b041ddfddf40d5972
Martin Boer <martin.boer@wur.nl>
2025-02-05 19:35:20 +0100

    simulated example torus for both gaussian and poisson data

--- examples/torus_example.R
+++ examples/torus_example.R
@@ -11,21 +11,35 @@ suppressMessages(library(spam))
 
 set.seed(1234)
 
-sim_fun <- function(x) {exp(sin(2*pi*x[1]))+
-                             exp(cos(2*pi*x[2])) }
+cnt.data <- TRUE
+if (cnt.data) {
+  fam <- poisson()
+} else {
+  fam <- gaussian()
+}
+
+sim_fun <- function(x) {
+  y <- exp(sin(2*pi*x[1])) + exp(cos(2*pi*x[2]))
+  y
+}
 
 # simulate data on a grid:
 n1 <- 100
 n2 <- 100
 n <- n1*n2
 dat <- expand.grid(x1=seq(0,1,length=n1),x2=seq(0,1,length=n2))
-dat$ytrue <- apply(dat, MARGIN=1, FUN =sim_fun)
+dat$ytrue <- apply(dat, MARGIN=1, FUN = sim_fun)
 
 # take subset for training
 Ntraining <- 2500
-k <- sample(x=c(1:n), size=Ntraining)
+k <- sample(x=c(1:n), size = Ntraining)
 dat_train <- dat[k, ]
-dat_train$y <- dat_train$ytrue + rnorm(n=Ntraining)
+if (cnt.data) {
+  dat_train$y <- sapply(dat_train$ytrue,
+                        function(lambda) {rpois(n=1,lambda)})
+} else {
+  dat_train$y <- dat_train$ytrue + rnorm(n=Ntraining)
+}
 
 ggplot() +
   geom_tile(data = dat, aes(x=x1,y=x2,fill=ytrue)) +
@@ -35,18 +49,20 @@ ggplot() +
 ggplot() +
   geom_tile(data = dat_train, aes(x=x1,y=x2,fill=y)) +
   scale_fill_gradientn(colors = topo.colors(100)) +
-  coord_fixed() + ggtitle("Training: simulated plus noise") + JOPS_theme()
+  coord_fixed() + ggtitle("Training: simulated plus noise") +
+  JOPS_theme()
 
 nseg <- c(20,25)
 
 obj1 <- LMMsolve(fixed = y~1,
                  spline = ~torus(x1, x2, nseg),
-                 family = gaussian(),
+                 family = fam,
                  data = dat_train)
 summary(obj1)
 
 # prediction by foot, values should be in [0, 1]
-grid <- expand.grid(x1=seq(0,1,length=100),x2=seq(0,1,length=100))
+grid <- expand.grid(x1 = seq(0,1,length=100),
+                    x2 = seq(0,1,length=100))
 x1 <- grid$x1
 x2 <- grid$x2
 knots <- obj1$splRes[[1]]$knots
@@ -70,7 +86,7 @@ U <- cbind(X, B12)
 a <- obj1$coefMME
 yhat <- U %*% a
 pred <- dat
-pred$ypred <- yhat
+pred$ypred <- fam$linkinv(yhat)
 
 ggplot() +
   geom_tile(data = pred, aes(x=x1,y=x2, fill = ypred)) +
