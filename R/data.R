886d0674be484e73e8953c1b4bd49f7de787cded
Martin Boer <martin.boer@wur.nl>
2025-11-16 19:10:18 +0100

    The barley uniformity trial added

--- R/data.R
+++ R/data.R
@@ -41,4 +41,18 @@
 #' \doi{10.1146/annurev-statistics-040120-020733}
 "SeaSurfaceTemp"
 
+#' Uniformity trial of barley
+#' @format A data.frame with 1076 rows and 3 columns
+#' \describe{
+#'  \item{row}{row coordinate}
+#'  \item{col}{column coordinate}
+#'  \item{yield}{yield per plot}
+#' }
+#' @source H. P. Piepho & E. R. Williams (2010). Linear variance models
+#' for plant breeding trials. Plant Breeding, 129, 1-8.
+#' \doi{/doi.org/10.1111/j.1439-0523.2009.01654.x}
+#' @references Piepho, Hans‐Peter, Martin P. Boer, and Emlyn R. Williams.
+#' "Two‐dimensional P‐spline smoothing for spatial analysis of plant breeding trials."
+#' Biometrical Journal 64, no. 5 (2022): 835-857.
+"barley.uniformity.trial"
 
